052983ebc559dc1b561405c8a82ba0a3c42590b2
Pranshu Chittora <pranshu@signoz.io>
2022-07-12 16:57:15 +0530

    fix: revert URI change

--- src/2-send-trace-to-collector.php
+++ src/2-send-trace-to-collector.php
@@ -18,7 +18,7 @@ use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
 use OpenTelemetry\SDK\Trace\TracerProvider;
 use OpenTelemetry\API\Trace\SpanKind;
 
-putenv('OTEL_EXPORTER_OTLP_ENDPOINT=http://stagingapp.signoz.io:4318/v1/traces');
+putenv('OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4318/v1/traces');
 putenv('OTEL_SERVICE_NAME=signoz-php-app');
 
 $exporter = new OTLPExporter(
