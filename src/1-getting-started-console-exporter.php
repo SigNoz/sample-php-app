7fb8892ae36bb2bbe6b0386864125da0422d9db1
Brett McBride <brett@deakin.edu.au>
2023-02-03 17:23:28 +1100

    update to use and work with opentelemetry-php beta release

--- src/1-getting-started-console-exporter.php
+++ src/1-getting-started-console-exporter.php
@@ -9,7 +9,7 @@ require __DIR__ . '/../vendor/autoload.php';
 // Traces not not being sent to any collector/service, though they are outputted in the console.
 // References - https://github.com/open-telemetry/opentelemetry-php
 
-use OpenTelemetry\SDK\Trace\SpanExporter\ConsoleSpanExporter;
+use OpenTelemetry\SDK\Trace\SpanExporter\ConsoleSpanExporterFactory;
 use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
 use OpenTelemetry\SDK\Trace\TracerProvider;
 use OpenTelemetry\API\Trace\SpanKind;
@@ -18,20 +18,20 @@ echo 'Starting ConsoleSpanExporter' . PHP_EOL;
 
 $tracerProvider =  new TracerProvider(
     new SimpleSpanProcessor(
-        new ConsoleSpanExporter()
+        (new ConsoleSpanExporterFactory())->create()
     )
 );
 
 $tracer = $tracerProvider->getTracer('io.opentelemetry.contrib.php',);
 
 $rootSpan = $tracer->spanBuilder('root')->setSpanKind(SpanKind::KIND_SERVER)->startSpan();
-$rootSpan->activate();
+$rootScope = $rootSpan->activate();
 
 try {
     $span1 = $tracer->spanBuilder('foo')->setSpanKind(SpanKind::KIND_SERVER)->startSpan();
 
     print_r($span1);
-    $span1->activate();
+    $scope1 = $span1->activate();
     try {
         $span2 = $tracer->spanBuilder('bar')->setSpanKind(SpanKind::KIND_SERVER)->startSpan();
         echo 'OpenTelemetry welcomes PHP' . PHP_EOL;
@@ -40,5 +40,7 @@ try {
     }
 } finally {
     $span1->end();
+    $scope1->detach();
 }
 $rootSpan->end();
+$rootScope->detach();
--- src/2-send-trace-to-collector.php
+++ src/2-send-trace-to-collector.php
@@ -17,27 +17,16 @@ use OpenTelemetry\SDK\Common\Attribute\Attributes;
 use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
 use OpenTelemetry\SDK\Trace\TracerProvider;
 use OpenTelemetry\API\Trace\SpanKind;
+use OpenTelemetry\SDK\Trace\TracerProviderFactory;
 
 putenv('OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4318/v1/traces');
 putenv('OTEL_SERVICE_NAME=signoz-php-app');
 
-$exporter = new OTLPExporter(
-    new Client(),
-    new HttpFactory(),
-    new HttpFactory()
-);
-
-echo 'Starting OTLPExample';
-
-$tracerProvider =  new TracerProvider(
-    new SimpleSpanProcessor(
-        $exporter
-    )
-);
-$tracer = $tracerProvider->getTracer();
+$tracerProvider = (new TracerProviderFactory())->create();
+$tracer = $tracerProvider->getTracer('io.signoz.php.example');
 
 $root = $span = $tracer->spanBuilder('root')->setSpanKind(SpanKind::KIND_SERVER)->startSpan();
-$span->activate();
+$rootScope = $span->activate();
 
 for ($i = 0; $i < 3; $i++) {
     // start a span, register some events
@@ -46,17 +35,18 @@ for ($i = 0; $i < 3; $i++) {
     $span->setAttribute('remote_ip', '1.2.3.4')
         ->setAttribute('country', 'USA');
 
-    $span->addEvent('found_login' . $i, new Attributes([
+    $span->addEvent('found_login' . $i, [
         'id' => $i,
         'username' => 'otuser' . $i,
-    ]));
-    $span->addEvent('generated_session', new Attributes([
+    ]);
+    $span->addEvent('generated_session', [
         'id' => md5((string) microtime(true)),
-    ]));
+    ]);
 
     $span->end();
 }
 $root->end();
+$rootScope->detach();
 echo PHP_EOL . 'OTLPExample complete!  ';
 
 echo PHP_EOL;
